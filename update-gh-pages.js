b2323c797355358444ac8debc3e6f52b70ca0698
Jeremy Rose <nornagon@nornagon.net>
2021-02-01 12:59:46 -0800

    master -> main in update-gh-pages.js

--- update-gh-pages.js
+++ update-gh-pages.js
@@ -7,7 +7,7 @@ const dist = path.join(__dirname, 'dist')
 spawnSync('git', ['-C', dist, 'init'], {stdio: 'inherit'})
 spawnSync('git', ['-C', dist, 'add', '.'], {stdio: 'inherit'})
 spawnSync('git', ['-C', dist, 'commit', '-m', 'update'], {stdio: 'inherit'})
-spawnSync('git', ['-C', __dirname, 'fetch', dist, '-f', 'master:gh-pages'], {stdio: 'inherit'})
+spawnSync('git', ['-C', __dirname, 'fetch', dist, '-f', 'main:gh-pages'], {stdio: 'inherit'})
 
 // TODO: use a tmpdir for .git?
 fs.rmdirSync(path.join(dist, '.git'), {recursive: true})

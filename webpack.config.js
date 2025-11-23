0da792cba4d34004caea20642a1206247c60705b
Jeremy Rose <nornagon@nornagon.net>
2025-11-23 13:28:57 -0800

    pretty print data

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,35 @@
 const path = require('path')
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    static: './dist',
-    allowedHosts: 'all',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ],
-      },
-      {
-        test: /\.(png|jpg)$/,
-        use: [ 'file-loader' ],
-      },
-    ],
-  },
+module.exports = (_, argv = {}) => {
+  const mode = argv.mode || process.env.NODE_ENV || 'development'
+  const isProd = mode === 'production'
+
+  return {
+    mode,
+    entry: './src/index.js',
+    output: {
+      filename: '[name].bundle.js',
+      chunkFilename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+      static: './dist',
+      allowedHosts: 'all',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|jpg)$/,
+          use: ['file-loader'],
+        },
+      ],
+    },
+    optimization: {
+      minimize: isProd,
+    },
+  }
 }
